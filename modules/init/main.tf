2faf2b610e12527d967f93d41f089df45e9c7893
ValtGun <guntis.valters@gmail.com>
2022-05-15 06:06:40 -0700

    Fixes for S3 bucket storage

--- modules/init/main.tf
+++ modules/init/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "GV-terraform-up-and-running-state"
+  bucket = "gv-terraform-state"
 }
 
 resource "aws_s3_bucket_versioning" "example" {
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "GV-terraform-up-and-running-locks"
+  name         = "gv-terraform-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
--- services/dev/nginx-web-server/main.tf
+++ services/dev/nginx-web-server/main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "GV-terraform-up-and-running-state"
+    bucket         = "gv-terraform-state"
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-1"
     # Replace this with your DynamoDB table name!
-    dynamodb_table = "GV-terraform-up-and-running-locks"
+    dynamodb_table = "gv-terraform-locks"
     encrypt        = true
   }
 }

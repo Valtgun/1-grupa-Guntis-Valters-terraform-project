ef13bd5b0313b72db877cdf091e8ef49b0b47ed1
ValtGun <guntis.valters@gmail.com>
2022-05-15 05:51:41 -0700

    Added tf config to use S3 state

--- services/dev/nginx-web-server/main.tf
+++ services/dev/nginx-web-server/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "GV-terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "GV-terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
 module "aws" {
   source = "../../../modules/aws"
 }
